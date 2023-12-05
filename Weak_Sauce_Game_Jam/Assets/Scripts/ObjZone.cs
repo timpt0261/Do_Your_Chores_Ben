1104661c4c21dee4f35bee103ac2c97d477a3c50
Steven <stevenren999@gmail.com>
2023-12-04 20:35:36 -0800

    fixed and cleaned obj zone

--- Weak_Sauce_Game_Jam/Assets/Scripts/ObjZone.cs
+++ Weak_Sauce_Game_Jam/Assets/Scripts/ObjZone.cs
@@ -4,44 +4,17 @@ using UnityEngine;
 
 public class ObjZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            //Debug.Log("Player is in the objective zone");
-        } else if (other.gameObject.tag == "Interactable")
+        if (other.gameObject.tag == "Toy")
         {
             ScoreManager.Instance.addScore();
         }
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            //Debug.Log("Player is in the objective zone");
-        }
-    }
-
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            //Debug.Log("Player has left the objective zone");
-        }
-        else if (other.gameObject.tag == "Interactable")
+        if (other.gameObject.tag == "Toy")
         {
             ScoreManager.Instance.subtractScore();
         }
