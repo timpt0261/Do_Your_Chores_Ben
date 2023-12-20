f6f98d9bd6917ae1d82f6013642769db4dbf11ca
timpt0261 <rchavez0215@gmail.com>
2023-12-19 22:10:48 -0800

    Updated ThirdPersonController Package

--- Weak_Sauce_Game_Jam/Assets/InputSystem/StarterAssetsInputs.cs
+++ Weak_Sauce_Game_Jam/Assets/InputSystem/StarterAssetsInputs.cs
@@ -1,98 +1,90 @@
 using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
 
 namespace StarterAssets
 {
-	public class StarterAssetsInputs : MonoBehaviour
-	{
-		[Header("Character Input Values")]
-		[SerializeField]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+    public class StarterAssetsInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        [SerializeField]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
+        public bool interact; // Custom interaction for player: Reuben Chavez
 
-		// Custom interaction for player : Reuben Chavez
-		public bool interact;
+        [Header("Movement Settings")]
+        public bool analogMovement;
 
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
+#if ENABLE_INPUT_SYSTEM
+        public void OnMove(InputAction.CallbackContext value)
+        {
+            MoveInput(value.ReadValue<Vector2>());
+        }
 
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+        public void OnLook(InputAction.CallbackContext value)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(value.ReadValue<Vector2>());
+            }
+        }
 
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-		public void OnMove(InputAction.CallbackContext value)
-		{
-			MoveInput(value.ReadValue<Vector2>());
-		}
+        public void OnJump(InputAction.CallbackContext value)
+        {
+            JumpInput(value.action.triggered);
+        }
 
-		public void OnLook(InputAction.CallbackContext value)
-		{
-			if (cursorInputForLook)
-			{
-				LookInput(value.ReadValue<Vector2>());
-			}
-		}
+        public void OnSprint(InputAction.CallbackContext value)
+        {
+            SprintInput(value.ReadValue<float>() == 1);
+        }
 
-		public void OnJump(InputAction.CallbackContext value)
-		{
-			JumpInput(value.action.triggered);
-		}
-
-		public void OnSprint(InputAction.CallbackContext value)
-		{
-			SprintInput(value.ReadValue<float>() == 1);
-		}
-
-		public void OnInteract(InputAction.CallbackContext value)
-		{
-			InteractInput(value.action.triggered);
-
-		}
-
-
+        public void OnInteract(InputAction.CallbackContext value)
+        {
+            InteractInput(value.action.triggered);
+        }
 #endif
-		public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		}
 
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
+        public void MoveInput(Vector2 newMoveDirection)
+        {
+            move = newMoveDirection;
+        }
 
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
 
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
 
-	
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
 
-		public void InteractInput(bool newInteractState)
-		{
-			interact = newInteractState;
-		}
+        public void InteractInput(bool newInteractState)
+        {
+            interact = newInteractState;
+        }
 
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
 
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
-	}
-
+        private void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+    }
 }
